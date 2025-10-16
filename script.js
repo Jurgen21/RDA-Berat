ca2dbf98468d2d8c5161264c223eae0e30ab7657
Jurgen21 <152924878+Jurgen21@users.noreply.github.com>
2025-10-16 11:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -104,12 +104,12 @@ window.addEventListener('scroll', () => {
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if(entry.isIntersecting) {
+    if (entry.isIntersecting) {
       entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
+      observer.unobserve(entry.target);
     }
   });
 });
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
